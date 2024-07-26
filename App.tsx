69b3cff206572cffec5073f410f46af5b5bdc3da
Alex <king.zan@hotmail.com>
2024-07-26 18:10:27 -0500

    status bar blends

--- App.tsx
+++ App.tsx
@@ -1,18 +1,30 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { WebView } from 'react-native-webview';
 import SplashScreen from 'react-native-splash-screen';
-
-// useEffect(() => {
-//   SplashScreen.hide();
-// }, []);
-//
+import { StatusBar, StyleSheet, View } from 'react-native';
 
 const App = () => {
-//   useEffect(() => {
-//   SplashScreen.hide();
-// }, []);
+  // useEffect(() => {
+  //   SplashScreen.hide();
+  // }, []);
 
-  return <WebView source={{ uri: 'https://rabbithole.fm' }} />;
+  return (
+    <View style={styles.container}>
+    <StatusBar animated={true} backgroundColor="#FFFFFF" barStyle="dark-content" />
+    <WebView source={{ uri: 'https://rabbithole.fm' }} style={styles.webview} />
+    </View>
+  );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'transparent', // Set to your primary color
+  },
+  webview: {
+    flex: 1,
+    marginTop: Platform.OS === 'android' ? StatusBar.currentHeight : 0, // Add padding for status bar on Android
+  },
+});
 
 export default App;
